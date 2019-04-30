40c7487f76a0b089884cc63148c720dc6b2e7b5e
Damon Harris (TheDcoder) <TheDcoder@protonmail.com>
2019-04-30 17:06:16 +0530

    Implement a working algorithm compatible with tor

--- torhash.c
+++ torhash.c
@@ -1,9 +1,19 @@
 #include <plibsys.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-void clean_exit(bool error);
+// SHA1 digest length
+#define DIGEST_LEN 20
+
+// Length of RFC2440-style S2K specifier: the first 8 bytes are a salt, the
+// 9th describes how much iteration to do.
+#define S2K_RFC2440_SPECIFIER_LEN 9
+
+bool secret_to_key_rfc2440(char *key_out, size_t key_out_len, const char *secret, size_t secret_len, const char *s2k_specifier);
+void clean_exit();
+char *bin2hex(const unsigned char *bin, size_t len);
 
 int main(int argc, char *argv[]) {
 	// Check arguments
@@ -15,44 +25,112 @@ int main(int argc, char *argv[]) {
 	// Initialize plibsys
 	p_libsys_init();
 	
-	// Initialize cryptographic hash context
-	PCryptoHash *hash = p_crypto_hash_new(P_CRYPTO_HASH_TYPE_SHA1);
-	if (!hash) {
-		fputs("Failed to initialize cryptographic hash context!\n", stderr);
-		clean_exit(false);
-	}
-	bool hash_error = false;
+	// Seed the random number generator
+	srand(1337);
+	
+	// Allocate buffer for storing the key
+	char key[S2K_RFC2440_SPECIFIER_LEN + DIGEST_LEN];
 	
 	// Iterate over arguments
 	for (size_t arg_num = 1; arg_num < argc; ++arg_num) {
-		// Add data for hashing
-		p_crypto_hash_update(hash, (puchar *) argv[arg_num], strlen(argv[arg_num]));
+		// Populate the specifier with random information
+		// NOTE: This is not cryptographically secure and works with a fixed seed
+		for (size_t n = 0; n < S2K_RFC2440_SPECIFIER_LEN - 1; ++n) key[n] = (char) rand();
+		key[S2K_RFC2440_SPECIFIER_LEN - 1] = 96; // This is hard-coded in tor
 		
-		// Get the hash as a string
-		char *hash_string = p_crypto_hash_get_string(hash);
-		if (!hash_string) {
-			fputs("Failed to compute hash!\n", stderr);
-			hash_error = true;
-			break;
+		// Hash the string
+		bool success = secret_to_key_rfc2440(
+			key + S2K_RFC2440_SPECIFIER_LEN,
+			DIGEST_LEN,
+			argv[arg_num],
+			strlen(argv[arg_num]),
+			key
+		);
+		if (!success) {
+			fputs("Failed to hash the string!\n", stderr);
+			continue;
 		}
 		
-		// Print the hash
-		puts(hash_string);
+		// Convert the hash to a hexadecimal string
+		char *hex_string = bin2hex((unsigned char *) key, S2K_RFC2440_SPECIFIER_LEN + DIGEST_LEN);
+		if (!hex_string) {
+			fputs("Failed to convert hash to hexadecimal string!\n", stderr);
+			continue;
+		}
 		
-		// Free the string
-		p_free(hash_string);
-		
-		// Reset the hash context
-		p_crypto_hash_reset(hash);
+		// Print the hex encoded key
+		printf("16:%s\n", hex_string);
+		free(hex_string);
 	}
 	
 	// Perform a clean exit
-	p_crypto_hash_free(hash);
-	clean_exit(!hash_error);
+	clean_exit();
 }
 
-void clean_exit(bool no_error) {
+bool secret_to_key_rfc2440(char *key_out, size_t key_out_len, const char *secret, size_t secret_len, const char *s2k_specifier) {
+	char iteration_count = s2k_specifier[S2K_RFC2440_SPECIFIER_LEN - 1];
+	#define EXPBIAS 6
+	size_t count = ((uint32_t)16 + (iteration_count & 15)) << ((iteration_count >> 4) + EXPBIAS);
+	#undef EXPBIAS
+	
+	// Allocate and populate the temporary buffer with data
+	// This is the data which will be hashed
+	char *temp = malloc((S2K_RFC2440_SPECIFIER_LEN - 1) + secret_len);
+	memcpy(temp, s2k_specifier, S2K_RFC2440_SPECIFIER_LEN - 1);
+	memcpy(temp + (S2K_RFC2440_SPECIFIER_LEN - 1), secret, secret_len);
+	secret_len += S2K_RFC2440_SPECIFIER_LEN - 1;
+	
+	// Hash the data
+	PCryptoHash *hash = p_crypto_hash_new(P_CRYPTO_HASH_TYPE_SHA1);
+	if (!hash) {
+		fputs("Failed to initialize cryptographic hash context!\n", stderr);
+		return false;
+	}
+	while (count != 0) {
+		if (count >= secret_len) {
+			p_crypto_hash_update(hash, (puchar *) temp, secret_len);
+			count -= secret_len;
+		} else {
+			p_crypto_hash_update(hash, (puchar *) temp, count);
+			count = 0;
+		}
+	}
+	free(temp);
+	
+	// Get the raw digest
+	size_t digest_len = DIGEST_LEN;
+	unsigned char digest[DIGEST_LEN];
+	p_crypto_hash_get_digest(hash, digest, &digest_len);
+	p_crypto_hash_free(hash);
+	
+	// Copy the digest
+	if (key_out_len <= digest_len) {
+		memcpy(key_out, digest, key_out_len);
+		return true;
+	} else {
+		// Key expansion is unsupported at the moment
+		return false;
+	}
+}
+
+void clean_exit() {
 	// Clean up and exit
 	p_libsys_shutdown();
-	exit(no_error ? EXIT_SUCCESS : EXIT_FAILURE);
+	exit(EXIT_SUCCESS);
+}
+
+// Forked from: https://nachtimwald.com/2017/09/24/hex-encode-and-decode-in-c/
+char *bin2hex(const unsigned char *bin, size_t len) {
+	if (bin == NULL || len == 0) return NULL;
+	
+	char *out = malloc(len*2+1);
+	if (!out) return NULL;
+	
+	for (size_t i=0; i<len; i++) {
+		out[i*2]   = "0123456789ABCDEF"[bin[i] >> 4];
+		out[i*2+1] = "0123456789ABCDEF"[bin[i] & 0x0F];
+	}
+	out[len*2] = '\0';
+	
+	return out;
 }
